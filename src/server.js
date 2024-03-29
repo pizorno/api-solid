978d16805a6cf7f3c206363646bb9ff25f60da93
Eduardo Pizorno <pizorno@gmail.com>
2024-03-29 19:03:55 -0300

    fix: package.json

--- src/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("./app");
-app_1.app.listen({
-    host: '0.0.0.0',
-    port: 3333
-}).then(function () {
-    console.log('🚀 HTTP Server Running');
-});
