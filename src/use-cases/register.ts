853ce70566c21b71f96a4290aad5f96863365313
Eduardo Pizorno <pizorno@gmail.com>
2024-04-07 10:32:04 -0300

    implementing error handlers

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,6 @@
 import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -12,6 +13,11 @@ export class RegisterUseCase {
 
   async execute({ name, email, password }: RegisterUseCaseRequest) {
     const passwordHash = await hash(password, 6)
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
+    }
 
     await this.usersRepository.create({
       name,
